130b795d3e9ea9536b4d2b18658ed00c4c710083
Victor <victor.dubret@epitech.eu>
2018-06-13 14:13:58 +0200

    [+] getter & nbOfactions get the range

--- Ai/Ai.py
+++ Ai/Ai.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from Ai import Tile
+from Ai import PathFinding
 
 
 class Target(Enum):
@@ -8,11 +9,13 @@ class Target(Enum):
     STONE = 2
     PLAYER = 3
 
+
 class Direction(Enum):
     NORTH = 0
     SOUTH = 1
     EAST = 2
     WEST = 3
+
 
 class Ai:
 
@@ -20,17 +23,23 @@ class Ai:
         self.__inventory = {"food": 10, "linemate": 0, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0,
                             "thystame": 0}
         self.__map = [[Tile.Tile() for _ in range(x)] for _ in range(y)]
-        self.__coord = [0, 0]
-        self.__Dir = Direction.SOUTH
+        self.__coord = [9, 0]
+        self.__dir = Direction.SOUTH
         print(self.__map)
 
     def think(self):
         for key in self.__inventory:
             print("Key : %s --> value %d" % (key, self.__inventory[key]))
         print(self.__inventory)
+        toto = PathFinding.PathFinding()
 
-    def nbOfActions(self, x, y):
+        toto.nbOfActions(self.__coord, 8, 0)
 
+    def getMap(self):
+        return self.__map
 
+    def getCoord(self):
+        return self.__coord
 
-
+    def getInentory(self):
+        return self.__inventory
---
+++ Ai/PathFinding.py
@@ -0,0 +1,30 @@
+from Ai import Ai
+
+class PathFinding:
+    def __init__(self):
+        pass
+
+    def dist(self, player_coord, coord):
+        range = 0
+        nearest_dir = Ai.Direction.NORTH
+        print("coord_player : %d && coord : %d" % (player_coord, coord))
+        if player_coord > coord:
+            range = player_coord - coord
+        elif coord > player_coord:
+            nearest_dir = Ai.Direction.SOUTH
+            range = coord - player_coord
+        return range, nearest_dir
+
+    def nbOfActions(self, player_coords, x, y):
+        action_number = 0
+        nearest_direction = Ai.Direction.NORTH
+        range = 0
+        if player_coords[0] == x and player_coords[1] != y:  # Je suis en ligne droite Y
+            print("Je vais avancer sur les y")
+            range, nearest_direction = self.dist(player_coords[1], y)
+        elif player_coords[1] == y and player_coords[1] != x:  # Je suis en ligne droite X
+            range, nearest_direction = self.dist(player_coords[0], x)
+            nearest_direction = Ai.Direction.WEST if nearest_direction == Ai.Direction.NORTH else Ai.Direction.EAST
+        print(nearest_direction)
+        print("Range : %d" % range)
+        return action_number

e15e2872c7a60d6c47f43f2a29d2077bd90b935f
Victor <victor.dubret@epitech.eu>
2018-06-14 10:16:21 +0200

    [+] Diagonal move

--- Ai/PathFinding.py
+++ Ai/PathFinding.py
@@ -21,16 +21,17 @@ class PathFinding:
     def move(self, normal_range, opposite_range, player_dir):
         actions = []
         if normal_range[1] != player_dir and normal_range[0] + 2 < opposite_range[0]:
-            print("TITI")
             actions.append("Right")
             actions.append("Right")
+            player_dir = normal_range[1]
             for i in range(0, normal_range[0]):
                 actions.append("Forward")
         elif normal_range[0] <= opposite_range[0]:
             print("normal range : %d" % normal_range[0])
-            if normal_range[1] != player_dir:
+            '''if normal_range[1] != player_dir:  # Ne peux pas arriver
                 actions.append("Right")
                 actions.append("Right")
+                player_dir = normal_range[1]'''
             for i in range(normal_range[0]):
                 actions.append("Forward")
         else:
@@ -38,44 +39,57 @@ class PathFinding:
             if opposite_range[1] != player_dir:
                 actions.append("Right")
                 actions.append("Right")
+                player_dir = opposite_range[1]
             for i in range(opposite_range[0]):
                 actions.append("Forward")
-        return actions
+        return actions, player_dir
+
+    def moveUpOrDown(self, player_coords, to, player_dir):
+        normal_range = [0, Ai.Direction.NORTH]
+        print("Je vais avancer sur les y")
+        normal_range[0], normal_range[1] = self.dist(player_coords[1], to[1])
+        print("distance : %d, dir : %s" % (normal_range[0], normal_range[1]))
+        opposite_range = [
+            self.__mapsize_y - normal_range[0],
+            Ai.Direction.NORTH if normal_range[1] == Ai.Direction.SOUTH else Ai.Direction.SOUTH]
+        actions, player_dir = self.move(normal_range, opposite_range, player_dir)
+        return actions, player_dir
+
+    def moveLeftOrRight(self, player_coords, to, player_dir):
+        normal_range = [0, Ai.Direction.NORTH]
+        normal_range[0], normal_range[1] = self.dist(player_coords[0], to[0])
+        normal_range[1] = Ai.Direction.WEST if normal_range[1] == Ai.Direction.NORTH else Ai.Direction.EAST
+        opposite_range = [
+            self.__mapsize_x - normal_range[0],
+            Ai.Direction.EAST if normal_range[1] == Ai.Direction.WEST else Ai.Direction.WEST]
+        actions, player_dir = self.move(normal_range, opposite_range, player_dir)
+        return actions, player_dir
 
     def goToTile(self, player_coords, to, player_dir):
-        normal_range = [0, Ai.Direction.NORTH]
         nb_actions = 0
         actions = []
 
-        if player_coords[0] == to[0]:  # Je suis en ligne droite Y
-            print("Je vais avancer sur les y")
-            normal_range[0], normal_range[1] = self.dist(player_coords[1], to[1])
-            print("distance : %d, dir : %s" % (normal_range[0], normal_range[1]))
-            opposite_range = [
-                self.__mapsize_y - normal_range[0],
-                Ai.Direction.NORTH if normal_range[1] == Ai.Direction.SOUTH else Ai.Direction.SOUTH]
-            actions = self.move(normal_range, opposite_range, player_dir)
-            print(actions)
-        elif player_coords[1] == to[1]:  # Je suis en ligne droite X
-            normal_range[0], normal_range[1] = self.dist(player_coords[0], to[0])
-            normal_range[1] = Ai.Direction.WEST if normal_range[1] == Ai.Direction.NORTH else Ai.Direction.EAST
-            opposite_range = [
-                self.__mapsize_x - normal_range[0],
-                Ai.Direction.EAST if normal_range[1] == Ai.Direction.WEST else Ai.Direction.WEST]
-            actions = self.move(normal_range, opposite_range, player_dir)
-            print(actions)
-
-'''    def nbOfActions(self, player_coords, x, y):
-        action_number = 0
-        nearest_direction = Ai.Direction.NORTH
-        range = 0
-        if player_coords[0] == x and player_coords[1] != y:  # Je suis en ligne droite Y
-            print("Je vais avancer sur les y")
-            range, nearest_direction = self.dist(player_coords[1], y)
-        elif player_coords[1] == y and player_coords[1] != x:  # Je suis en ligne droite X
-            range, nearest_direction = self.dist(player_coords[0], x)
-            nearest_direction = Ai.Direction.WEST if nearest_direction == Ai.Direction.NORTH else Ai.Direction.EAST
-        print(nearest_direction)
-        print("Range : %d" % range)
-        return action_number
-'''
+        if player_coords[0] != to[0] and player_coords[1] != to[1]:  # Diagonales
+            if player_dir == Ai.Direction.NORTH or player_dir == Ai.Direction.SOUTH:
+                actions, player_dir = self.moveUpOrDown(player_coords, (player_coords[0], to[1]), player_dir)
+                player_dir = Ai.Direction.EAST #TODO Bifurcation
+                actions.append("Right")
+                player_coords[1] = to[1]
+                tmp, player_dir = self.moveLeftOrRight(player_coords, to, player_dir)
+                actions += tmp
+                print(actions)
+            else:
+                actions, player_dir = self.moveUpOrDown(player_coords, (to[0], player_coords[1]), player_dir)
+                actions.append("Right")
+                player_dir = Ai.Direction.SOUTH
+                player_coords[0] = to[0]
+                tmp, player_dir = self.moveLeftOrRight(player_coords, to, player_dir)
+                actions += tmp
+                print(actions)
+        else:
+            if player_coords[0] == to[0]:  # Je suis en ligne droite Y
+                actions, player_dir = self.moveUpOrDown(player_coords, to, player_dir)
+                print(actions)
+            elif player_coords[1] == to[1]:  # Je suis en ligne droite X
+                actions, player_dir = self.moveLeftOrRight(player_coords, to, player_dir)
+                print(actions)
